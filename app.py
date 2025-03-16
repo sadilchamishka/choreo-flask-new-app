82111340949e81e7f04d1721866c9b799d079028
sadilchamishka <sandilchamishka@gmail.com>
2025-03-16 14:04:44 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,55 +1,43 @@
 from flask import Flask, jsonify, request
+import pickle
+import pandas as pd
+import tensorflow as tf
+import joblib
 
 app = Flask(__name__)
 
-books = []
+@app.route('/kidney-stone/predict', methods=['GET'])
+def predict():
+    try:
+        # List of expected parameters
+        params = ['param1', 'param2', 'param3', 'param4', 'param5', 'param6']
+        scaler = pickle.load(open("scaler.pickle", 'rb'))
 
-# helper function to generate IDs
-def generate_id():
-    return len(books) + 1
+        # Fetching parameters from request and validating if they are present
+        data_list = []
+        for param in params:
+            value = request.args.get(param)
+            if value is None:
+                return f"Error: Missing query parameter '{param}'"
+            try:
+                data_list.append(float(value))
+            except ValueError:
+                return f"Error: Query parameter '{param}' must be a valid number"
 
-# endpoint to get all books
-@app.route('/reading-list/books', methods=['GET'])
-def get_books():
-    return jsonify({'books': books})
+        # Reshaping the list to match the expected input format
+        df_new = pd.DataFrame([data_list])
+        df_new = scaler.transform(df_new)
 
-# endpoint to add a new book
-@app.route('/reading-list/books', methods=['POST'])
-def add_book():
-    data = request.get_json()
-    book = {
-        'id': generate_id(),
-        'author': data['author'],
-        'name': data['name'],
-        'status': data['status']
-    }
-    books.append(book)
-    return jsonify(book)
+       
+        
+        # Load the model
+        loaded_model1 = tf.keras.models.load_model('my_model.h5')
 
-# endpoint to get a book by ID
-@app.route('/reading-list/books/<int:book_id>', methods=['GET'])
-def get_book(book_id):
-    for book in books:
-        if book['id'] == book_id:
-            return jsonify(book)
-    return '', 404
+        # Predict the probability
+        return str(loaded_model1.predict(df_new)[0][0])
 
-# endpoint to delete a book by ID
-@app.route('/reading-list/books/<int:book_id>', methods=['DELETE'])
-def delete_book(book_id):
-    global books
-    books = [book for book in books if book['id'] != book_id]
-    return '', 204
-
-# endpoint to update a book status by ID
-@app.route('/reading-list/books/<int:book_id>', methods=['PUT'])
-def update_book_status(book_id):
-    data = request.get_json()
-    for book in books:
-        if book['id'] == book_id:
-            book['status'] = data['status']
-            return jsonify(book)
-    return '', 404
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 if __name__ == '__main__':
     app.run()
